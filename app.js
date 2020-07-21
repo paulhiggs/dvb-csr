b57e99f2e18fb27c572ffb7895eddfc5f7ea8594
PaulHiggs <paul_higgs@hotmail.com>
2020-07-21 13:20:56 +0100

    updates

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ const allowed_arguments = ['ProviderName', 'regulatorListFlag', 'Language', 'Tar
 const ISO3166_FILE=path.join('dvb-common','iso3166-countries.json');
 var knownCountries = new ISOcountries(false, true);
 
-const IANAlanguages = require("./dvb-common/IANAlanguages.js");
+const IANAlanguages = require('./dvb-common/IANAlanguages.js');
 
 // curl from https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
-const IANA_Subtag_Registry_Filename=path.join("./dvb-common","language-subtag-registry");
+const IANA_Subtag_Registry_Filename=path.join('./dvb-common','language-subtag-registry');
 var knownLanguages = new IANAlanguages();
 
 
@@ -49,12 +49,12 @@ app.use(morgan(':remote-addr :protocol :method :url :status :res[content-length]
 
 
 function isIn(args, value){
-	if (typeof(args) == "string")
+	if (typeof(args)=="string")
 		return args==value;
 	
-	if (typeof(args) == "object") {
+	if (typeof(args)=="object") {
 		for (var x=0; x<args.length; x++) 
-			if (args[x] == value)
+			if (args[x]==value)
 				return true;
 	}
 	return false;
@@ -66,25 +66,23 @@ app.get('/query', function(req,res){
 		res.status(400);
 	}
 	else {
-		var slepr = libxml.parseXmlString(masterSLEPR);
+		var slepr=libxml.parseXmlString(masterSLEPR);
 
-		var SLEPR_SCHEMA = {}, SCHEMA_PREFIX=slepr.root().namespace().prefix();
-		SLEPR_SCHEMA[slepr.root().namespace().prefix()]=slepr.root().namespace().href();
+		var SLEPR_SCHEMA={}, SCHEMA_PREFIX=slepr.root().namespace().prefix();
+		SLEPR_SCHEMA[SCHEMA_PREFIX]=slepr.root().namespace().href();
 			
 		if (req.query.ProviderName) {
 			// if ProviderName is specified, remove any ProviderOffering entries that do not match the name
-			var prov, p=1, providerCleanup = [];
+			var prov, p=1, providerCleanup=[];
 			while (prov=slepr.get('//'+SCHEMA_PREFIX+':ProviderOffering['+p+']', SLEPR_SCHEMA)) {
 				var provName, n=1, matchedProvider=false;
-				while ((provName=prov.get('//'+SCHEMA_PREFIX+':ProviderOffering['+p+']/'+SCHEMA_PREFIX+':Provider/'+SCHEMA_PREFIX+':Name['+n+']', SLEPR_SCHEMA)) && !matchedProvider) {
-					if (isIn(req.query.ProviderName, provName.text())) {
-						matchedProvider=true;
-					}						
+				while ((provName=prov.get(SCHEMA_PREFIX+':Provider/'+SCHEMA_PREFIX+':Name['+n+']', SLEPR_SCHEMA)) && !matchedProvider) {
+					if (isIn(req.query.ProviderName, provName.text())) 
+						matchedProvider=true;					
 					n++;
 				}
-				if (!matchedProvider) {
+				if (!matchedProvider) 
 					providerCleanup.push(prov);
-				}
 				p++;
 			}
 			providerCleanup.forEach(provider => provider.remove());
@@ -146,10 +144,10 @@ app.get('/query', function(req,res){
 			servicesToRemove.forEach(service => service.remove());
 		}
 			
-		// remove any <ProviderOffering> that no longer have any <ServiceListOffering>
+		// remove any <ProviderOffering> elements that no longer have any <ServiceListOffering>
 		var prov, p=1, providersToRemove=[];
 		while (prov=slepr.get('//'+SCHEMA_PREFIX+':ProviderOffering['+p+']', SLEPR_SCHEMA)) {
-			if (!slepr.get('//'+SCHEMA_PREFIX+':ProviderOffering['+p+']/'+SCHEMA_PREFIX+':ServiceListOffering[1]', SLEPR_SCHEMA)) 
+			if (!prov.get(SCHEMA_PREFIX+':ServiceListOffering[1]', SLEPR_SCHEMA)) 
 				providersToRemove.push(prov);
 			p++;
 		}
@@ -166,55 +164,56 @@ function isTVAAudioLanguageType(languageCode) {
 	// any language specified should be an XML language
 	var languageRegex=/[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*/g;
 	var s=languageCode.match(languageRegex);
-	return s?s[0] === languageCode:false;
+	return s?s[0]===languageCode:false;
 }
 
-
-function isGenre(genre) {
-	// DVB-I Genre is defined through classification schemes
-	// permitted values through TVA:ContentCS, TVA:FormatCS, DVB-I:ContentSubject 
-	return true;
-}
-
-function isProvider(provider) {
-	return true;
-}
 
 function checkQuery(req) {
+	
+/*	function isGenre(genre) {
+		// DVB-I Genre is defined through classification schemes
+		// permitted values through TVA:ContentCS, TVA:FormatCS, DVB-I:ContentSubject 
+		return true;
+	}
+
+	function isProvider(provider) {
+		return true;
+	} */
+	
 	if (req.query) {
 		
 		// check for any erronous arguments
 		for (key in req.query) {
 			if (!isIn(allowed_arguments, key)) {
-				req.parseErr = "invalid argument [" + key +"]";
+				req.parseErr="invalid argument ["+key+"]";
 				return false;
 			}
 		}
 		
 		//regulatorListFlag needs to be a boolean, "true" or "false" only
 		if (req.query.regulatorListFlag) {
-			if (typeof(req.query.regulatorListFlag) != "string" ) {
-				req.parseErr = "invalid type for regulatorListFlag ["+typeof(req.query.regulatorListFlag)+"]";
+			if (typeof(req.query.regulatorListFlag)!="string") {
+				req.parseErr="invalid type for regulatorListFlag ["+typeof(req.query.regulatorListFlag)+"]";
 				return false;
 			}
-			if (req.query.regulatorListFlag != "true" && req.query.regulatorListFlag != "false") {
-				req.parseErr = "invalid value for regulatorListFlag ["+req.query.regulatorListFlag+"]";
+			if (req.query.regulatorListFlag!="true" && req.query.regulatorListFlag!="false") {
+				req.parseErr="invalid value for regulatorListFlag ["+req.query.regulatorListFlag+"]";
 				return false;				
 			}
 		}
 		
 		//TargetCountry(s)
 		if (req.query.TargetCountry) {
-			if (typeof(req.query.TargetCountry)=="string"){
+			if (typeof(req.query.TargetCountry)=="string") {
 				if (!knownCountries.isISO3166code(req.query.TargetCountry)) {
-					req.parseErr = "incorrect country ["+req.query.TargetCountry+"]";
+					req.parseErr="incorrect country ["+req.query.TargetCountry+"]";
 					return false;
 				}					
 			}	
 			else if (typeof(req.query.TargetCountry)=="object") {
 				for (var i=0; i<req.query.TargetCountry.length; i++ ) {
 					if (!knownCountries.isISO3166code(req.query.TargetCountry[i])) {
-						req.parseErr = "incorrect country ["+req.query.TargetCountry[i]+"]";
+						req.parseErr="incorrect country ["+req.query.TargetCountry[i]+"]";
 						return false;
 					}
 				}
@@ -223,14 +222,16 @@ function checkQuery(req) {
 
 		//Language(s)
 		if (req.query.Language) {
-			if (typeof(req.query.Language)=="string"){
+			if (typeof(req.query.Language)=="string") {
 				if (!isTVAAudioLanguageType(req.query.Language)) {
+					req.parseErr="incorrect language ["+req.query.Language+"]";
 					return false;
 				}					
 			}	
 			else if (typeof(req.query.Language)=="object") {
 				for (var i=0; i<req.query.Language.length; i++ ) {
 					if (!isTVAAudioLanguageType(req.query.Language[i])) {
+						req.parseErr="incorrect language ["+req.query.Language[i]+"]";
 						return false;
 					}
 				}
@@ -272,10 +273,16 @@ function checkQuery(req) {
 	return true;
 }
 
-function loadServiceListRegistry() {
-	fs.readFile(MASTER_SLEPR_FILE, {encoding: 'utf-8'}, function(err,data){
+
+/**
+ * read in the master XML document as text
+ *
+ * @param {string} filename   filename of the master XML document
+ */
+function loadServiceListRegistry(filename) {
+	fs.readFile(filename, {encoding: 'utf-8'}, function(err,data){
 		if (!err) {
-			masterSLEPR = data.replace(/(\r\n|\n|\r|\t)/gm,"");
+			masterSLEPR=data.replace(/(\r\n|\n|\r|\t)/gm,"");
 		} else {
 			console.log(err);
 		}
@@ -283,7 +290,7 @@ function loadServiceListRegistry() {
 }
 
 app.get('/reload', function(req,res){
-	loadServiceListRegistry();
+	loadServiceListRegistry(MASTER_SLEPR_FILE);
 	knownCountries.loadCountriesFromFile(ISO3166_FILE, true);
 	res.status(200).end();
 });
@@ -293,12 +300,12 @@ app.get('*', function(req,res) {
 });
 
 
-loadServiceListRegistry();
+loadServiceListRegistry(MASTER_SLEPR_FILE);
 knownCountries.loadCountriesFromFile(ISO3166_FILE, true);
 knownLanguages.loadLanguagesFromFile(IANA_Subtag_Registry_Filename, true);
 
 // start the HTTP server
-var http_server = app.listen(HTTP_SERVICE_PORT, function() {
+var http_server=app.listen(HTTP_SERVICE_PORT, function() {
 	console.log("HTTP listening on port number", http_server.address().port);
 });
 
@@ -321,7 +328,7 @@ var https_options = {
 };
 
 if (https_options.key && https_options.cert) {
-	var https_server = https.createServer(https_options, app);
+	var https_server=https.createServer(https_options, app);
 	https_server.listen(HTTPS_SERVICE_PORT, function(){
 		console.log("HTTPS listening on port number", https_server.address().port);
 	});
