c1a6d7ed4100a08a757e4e3fd870c47df362fada
PaulHiggs <paul_higgs@hotmail.com>
2021-01-01 15:32:42 +0000

    a couple of template literals

--- app.js
+++ app.js
@@ -420,7 +420,7 @@ loadServiceListRegistry(options.file)
 
 // start the HTTP server
 var http_server=app.listen(options.port, function() {
-	console.log("HTTP listening on port number", http_server.address().port);
+	console.log(`HTTP listening on port number ${http_server.address().port}`);
 });
 
 
@@ -447,6 +447,6 @@ if (https_options.key && https_options.cert) {
 		
 	var https_server=https.createServer(https_options, app);
 	https_server.listen(options.sport, function(){
-		console.log("HTTPS listening on port number", https_server.address().port);
+		console.log(`HTTPS listening on port number ${https_server.address().port}`);
 	});
 }
